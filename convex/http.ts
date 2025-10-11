f722b89153d77554ccee15ef6637ab4f9e89a6b4
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-10-11 23:31:38 +0530

    small coomits

--- convex/http.ts
+++ convex/http.ts
@@ -1,7 +1,68 @@
-import { httpRouter, HttpRouter } from "convex/server";
-import {WebHookEvent} from "@clerk/nextjs/server";
+import { httpActionGeneric, httpRouter, HttpRouter } from "convex/server";
 import { Webhook } from "svix";
 import{api} from "./_generated/api";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { request } from "http";
+import {httpAction} from "./_generated/server";
+import { TemplateContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
+const http = httpRouter();
 
-const http = httpRouter()
+http.route({
+    path:"/clerk-webhook",
+    method:"POST",  
+    handler: httpAction(async (ctx,request) =>{
+        const webhookSecret= process.env.CLERK_WEBHOOK_SECRET;
+        if(!webhookSecret){
+            throw new Error("Missing CLERK_WEBHOOK_SECRET environment variable ");
+         }
+         const svix_id = request.headers.get("svix-id");
+         const svix_signature =  request.headers.get("svix-signature");
+         const svix_timestamp = request.headers.get("svix-timestamp");
+
+         if(!svix_id || !svix_signature || !svix_timestamp){
+            return new Response("No svix headers found",{
+                status: 400,
+            });
+         }
+         const payload = await request.json();
+         const body  = JSON.stringify(payload);
+
+         const wh = new Webhook(webhookSecret);
+         let evt: WebhookEvent;
+
+         try{
+            evt= wh.verify(body,{
+                "svix-id": svix_id,
+                "svix-timestamp": svix_timestamp,
+                "svix-signature": svix_signature,
+            }) as WebhookEvent;
+         } catch(err){
+            console.error("Error verifying webhook:", err);
+            return new Response("Error occured", {status: 400});
+         }
+
+         const eventType = evt.type;
+
+         if(eventType ==="user.created"){
+            const{id, first_name, image_url, email_addresses} = evt.data;
+            
+            const email = email_addresses[0].email_address;
+
+            const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+            try{
+                await ctx.runMutation(api.users.syncUser,{
+                    email,
+                    name,
+                    image: image_url,
+                    clerk_id: id
+                })
+            } catch(error){
+                console.log("Error creating user:", error);
+                return new Response("error creating user", {status: 500});
+            }
+         }
+
+    })
+})
 
