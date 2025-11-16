e101fd6d56bd37e6a14877a3d7dc09b7b5bed763
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-11-16 22:05:17 +0530

    hello

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
-import { Navbar } from "@/components/Navbar";
+import {Navbar} from "@/components/Navbar"
 import { Footer } from "@/components/Footer";
 
 const geistSans = Geist({
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,22 @@
+"use client";
 import React from 'react'
+import { useUser } from '@clerk/nextjs';
+import { ZapIcon } from 'lucide-react';
 
-export const Navbar = () => {
+const Navbar = () => {
+    const {isSignedIn} = useUser()
   return (
-    <div>Navbar</div>
+   
+      <header className='fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md border-b border-border py-3'>
+      <div className='container mx-auto flex items-center gap-2'>
+        <div className='p-1 bg-primary/10 rounded'>
+        <ZapIcon className='w-4 h-4 text-primary' />
+         
+        </div>
+
+      </div>
+      </header>
   )
 }
+
+export default Navbar;
