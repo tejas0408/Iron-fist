acfc54a0319c2e9ba0b1474d9632a0e876e97d76
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-11-28 22:45:39 +0530

    q

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ const Homepage = () => {
       <section className='realtive z-10 py-24 flex-grow'>
         <div className='container mx-auto px-4'>
           <div className='grid grid-cols-1 lg:grid-cols-12 gap-8 items-center relative'>
-
+          {/**Corner decoration */}
+          <div className='absolute -top-10 left-0 w-40 h-10 border-l-2 border-t-2 border-border'></div>
           </div>
 
         </div>
