db6802bb960ed1a8c1e007d4abe8abc68e06eee5
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-11-29 22:59:53 +0530

    qw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,9 @@ const Homepage = () => {
           </h1>
           {/*seperator line */}
           <div className='h-px w-full bg-gradient-to-r from-primary via-secondary to-primary opacity-50'></div>
-          
+          <p className='text-xl text-muted-foreground w-2/3'>
+          Talk to our AI assistant and get personalized diet plans and workout routines designed just for you
+          </p>
           </div>
           </div>
 
