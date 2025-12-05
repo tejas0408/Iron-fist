500279611c67fac5e946f1cac5fc1897ab20d59a
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-12-05 23:41:22 +0530

    dc

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 const GenerateProgramPage = () => {
+  const [callActive, setCallActive] = useState(false);
+  const [conncecting, setConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [callEnded, setCallEnded] = useState(false);
+
+  const { user } = useUser()
+  const router = useRouter()
+
   return (
     <div>GenerateProgramPage</div>
   )
