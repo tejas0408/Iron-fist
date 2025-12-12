1194211bdd86a528da3f736dc335c197a1b71621
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-12-12 22:00:13 +0530

    fd

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -170,21 +170,41 @@ const GenerateProgramPage = () => {
                   className={`w-2 h-2 rounded-full ${isSpeaking ? "bg-primary animate-pulse" : "bg-muted"}`} />
 
                 <span className="text-xs text-muted-foreground">
-                  {isSpeaking ? "Speaking..." : callActive ? "Listening..." : callEnded ? "Redirecting to profile..." : ""}
+                  {isSpeaking ? "Speaking..." : callActive ? "Listening..." : callEnded ? "Redirecting to profile..." : "Waiting..."}
                 </span>
-
-
-
               </div>
-
             </div>
           </Card>
+          {/*User Card */}
+          <Card className={`bg-card/90 backdrop-blur-sm border overflow-hidden relative`}>
+            <div className="aspect-video flex flex-col items-center justify-center p-6 relative">
+              {/* User Image */}
+              <div className="relative size-32 mb-4">
+                <img
+                  src={user?.imageUrl}
+                  alt="User"
+                  // ADD THIS "size-full" class to make it rounded on all images
+                  className="size-full object-cover rounded-full"
+                />
+              </div>
+              <h2 className="text-xl font-bold text-foreground">You</h2>
+              <p className="text-sm text-muted-foreground mt-1">
+                {user ? (user.firstName + " " + (user.lastName || "")).trim() : "Guest"}
+              </p>
 
-
+              {/* User Ready Text */}
+              <div className={`mt-4 flex items-center gap-2 px-3 py-1 rounded-full bg-card border`}>
+                <div className={`w-2 h-2 rounded-full bg-muted`} />
+                <span className="text-xs text-muted-foreground">Ready</span>
+              </div>
+            </div>
+          </Card>
         </div>
-      </div>
 
+      </div>
     </div>
+
+
   )
 }
 
