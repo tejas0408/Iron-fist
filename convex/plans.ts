71df4b9945586a90e8882f638432c106d0a93cd3
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-12-14 12:44:40 +0530

    xv

---
+++ convex/plans.ts
@@ -0,0 +1,37 @@
+import { mutation } from "./_generated/server"
+import { v } from "convex/values"
+
+export const createPlan = mutation({
+    args: {
+        userId: v.string(),
+        name: v.string(),
+        workoutPlan: v.object({
+            schedule: v.array(v.string()),
+            exercises: v.array(
+                v.object({
+                    day: v.string(),
+                    routines: v.array(
+                        v.object({
+                            name: v.string(),
+                            sets: v.number(),
+                            reps: v.number(),
+                        })
+                    ),
+                })
+            ),
+        }),
+        dietPlan: v.object({
+            dailyCalories: v.number(),
+            meals: v.array(
+                v.object({
+                    name: v.string(),
+                    foods: v.array(v.string()),
+                })
+            ),
+        }),
+        isActive: v.boolean(),
+    },
+    handler: async (ctx, args) => {
+
+    }
+})
