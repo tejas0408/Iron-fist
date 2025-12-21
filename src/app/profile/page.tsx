ecd84aa79ef97602747f070678ae0d7b23c0ac66
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-12-21 23:53:37 +0530

    c x

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,12 +4,17 @@ import { useUser } from "@clerk/nextjs"
 import { useQuery } from "convex/react";
 import { getUserPlans } from "../../../convex/plans";
 import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
 
 const ProfilePage = () => {
   const { user } = useUser();
   const userId = user?.id as string;
 
   const allPlans = useQuery(api.plans.getUserPlans, { userId });
+
+  const [selectedPlanId, setSelectedPlanId] = useState<null | string>(null);
+  const activePlan = allPlans?.find(plan => plan.isActive);
+  const currentPlan = selectedPlanId ? allPlans?.find(plan => plan._id === selectedPlanId) : activePlan;
 
   return <div>
   </div>;
