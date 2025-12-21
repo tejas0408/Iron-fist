a2cc443fb3c4c548f6e0a7964df358ea47518e24
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-12-21 23:59:09 +0530

    dfv

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { UserResource } from "@clerk/types";
 
-const ProfileHeader = () => {
+const ProfileHeader = ({ user }: { user: UserResource }) => {
     return (
-        <div>ProfileHeader</div>
+        <div>
+            <div className="flex items-center gap-4">
+                <img src={user.imageUrl} alt="" />
+                <div>
+                    <h1 className="text-2xl font-bold">{user.firstName} {user.lastName}</h1>
+                    <p className="text-gray-500">{user.email}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
