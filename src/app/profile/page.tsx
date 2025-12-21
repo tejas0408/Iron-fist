19bf0b71600eb196e4b72d8f66973643f09d45cf
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-12-21 23:45:21 +0530

    v

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,18 @@
 "use client"
 
+import { useUser } from "@clerk/nextjs"
+import { useQuery } from "convex/react";
+import { getUserPlans } from "../../../convex/plans";
+import { api } from "../../../convex/_generated/api";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { user } = useUser();
+  const userId = user?.id as string;
+
+  const allPlans = useQuery(api.plans.getUserPlans, { userId });
+
+  return <div>
+  </div>;
 
 }
 export default ProfilePage;
